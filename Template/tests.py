941bfba90f9a374311814c867de709362e2d425b
Harry Day <contact@harryday.xyz>
2023-08-07 18:48:17 +0100

    Add template folder

---
+++ Template/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from v1 import Solution as Solution
+
+
+class MyTestCase(unittest.TestCase):
+    def test_case_1(self):
+        pass
+
+    def test_case_2(self):
+        pass
+
+    def test_case_3(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
